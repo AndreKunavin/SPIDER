610706c1b3bdb73cc8d6ed9c8f284ed692817e51
AndreKunavin <kunavin.aa@phystech.edu>
2021-05-19 18:52:12 +0300

    header

---
+++ Voronoi_4/geom2.hpp
@@ -0,0 +1,149 @@
+#pragma once
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <SFML/Graphics.hpp>
+#include <random> 
+
+const double EPS = 1e-5;
+const double INF = 1e10;
+
+using std::istream;
+using std::ostream;
+
+bool in_interval(const double &a, const double &b, const double &x);
+
+
+
+struct Point : sf::Vector2<double>
+{
+	Point(const double& _x = INF, const double& _y = INF);
+
+	template <typename T>
+	Point(const Vector2<T>& v);
+    ///*****///
+    Point rand(double square);
+    ///*****///
+	double len() const;
+
+	Point normalize() const;
+
+	Point to_orthogonal() const;
+
+	Point move(const Point &v, const double &d) const;
+
+	Point operator-(const Point& b) const;
+
+	Point operator+(const Point& b) const;
+
+	Point operator*(const double& val) const;
+
+	bool operator==(const Point &b) const;
+
+	bool operator!=(const Point &b) const;
+
+	Point operator-() const;
+
+	friend istream& operator >> (std::istream& in, Point& p);
+	friend ostream& operator << (std::ostream &out, const Point &p);
+
+} const nunPoint(INF, INF);
+	
+typedef Point Vector;
+
+double len(Point a);
+
+double scalar_work(const Point &a, const Point &b);
+
+double vector_work(const Point &first, const Point &second);
+
+double angle(const Vector &v1, const Vector &v2);
+
+Point move(const Point &p, const Vector &v, double const &len);
+
+struct Line
+{
+	double a, b, c;
+
+	Line(const Point &A, const Point &B);
+
+	Line(const double &_a = 0, const double &_b = 0, const double &_c = 0);
+
+	virtual bool include(const Point &p) const;
+
+	friend istream& operator >> (std::istream &in, Line &line);
+
+
+	friend ostream& operator << (std::ostream &out, const Line &line);
+};
+
+double dist(const Line &line, const Point &p);
+
+struct Segment : public Line
+{
+	Point S, E;
+
+	Segment(const Point &S = 0, const Point &E = 0);
+    ///****///
+    Line midpoint_perpendicular();
+    ///****///
+	virtual bool include(const Point &p) const;
+
+	friend istream& operator >> (std::istream &in, Segment &seg);
+};
+
+double dist(const Segment &seg, const Point &p);
+
+struct Circle
+{
+	Point center;
+	double r;
+
+	bool operator==(const Circle &c) const;
+
+	bool operator!=(const Circle &c) const;
+
+	int include(const Point &p) const;
+
+	void get_tangent_points(const Point &p, Point &p1, Point &p2) const;
+
+	friend istream& operator >> (std::istream &in, Circle &circle);
+};
+
+typedef std::vector<Point> Polygon;
+
+
+std::istream& operator >> (std::istream& in, Point& p);
+
+std::ostream& operator << (std::ostream &out, const Point &p);
+
+std::istream& operator >> (std::istream &in, Line &line);
+
+std::ostream& operator << (std::ostream &out, const Line &line);
+
+std::istream& operator >> (std::istream &in, Circle &circle);
+
+std::istream& operator >> (std::istream &in, Segment &seg);
+
+std::ostream& operator << (std::ostream &out, const std::vector<Point> &p);
+
+
+Point get_intersection(const Line &l1, const Line &l2);
+
+Point get_intersection(const Segment& s1, const Segment& s2);
+
+std::vector<Point> get_intersection(const Circle &circle, const Line &line);
+
+std::vector<Point> get_intersection(Circle &c1, Circle &c2);
+///*****///
+#define BOARDER 999
+
+std::size_t line_point_att(Line l, Point s);
+
+std::size_t intersection(Line border, Point site, Point vertex);
+///*****///
+std::vector<Point> get_convex_hull(Polygon &v);
--- Voronoi_4/main.cpp
+++ Voronoi_4/main.cpp
@@ -1,100 +1,7 @@
-#include "geom.hpp"
+#include "geom2.hpp"
+#include "Voronoi.hpp"
 
-#define S_SIZE 1000
-#define SQUARE 100.0f
-
-class Cell {
-public:
-    Cell(Point &site_, Polygon &convex_) {
-        site = site_;
-        for (auto& it : convex_) {
-            convex.push_back(it);
-        }
-    }
-    Point site;
-    Polygon convex;
-};
-
-class Voronoi {
-public:
-    Voronoi(std::vector<Point> &site) {
-        plot_diagram(site);
-    }
-    std::vector<Cell> diagram;
-
-    void out() {
-        for (std::size_t i = 0; i < diagram.size(); i++) {
-            std::cout <<  "cell " << i << " (site " << diagram[i].site.x << ' ' << diagram[i].site.y << ")" << std::endl;
-            for (auto& v : diagram[i].convex) {
-                std::cout << v.x << ' ' << v.y << std::endl;
-            }
-            std::cout << std::endl;
-        }       
-    }
-
-private:   
-    void plot_diagram(std::vector<Point> &site) {
-
-        for (std::size_t i = 0; i < site.size(); i++) {
-            Polygon cell;
-            plot_cell(i, site, cell);
-        }
-
-    }
-    void plot_cell(std::size_t i, std::vector<Point> &site, Polygon &cell) {
-        
-        std::vector<Point> perps_intersection;
-        std::vector<Line> perps;
-        Polygon convex;
-        
-        for (std::size_t j = 0; j < site.size(); j++) {
-            if (i != j) {
-                Segment edge(site[i], site[j]);
-                perps.push_back(edge.midpoint_perpendicular());
-            }
-        }
-
-        for (std::size_t k = 0; k < perps.size(); k++) {
-            for (std::size_t j = k; j < perps.size(); j++) {
-                if (k != j) {
-                    Point p = get_intersection(perps[k], perps[j]);
-                    perps_intersection.push_back(p);
-                }
-            }
-        }
-
-        for (std::size_t j = 0; j < perps_intersection.size(); j++) {
-            //std::cout << perps_intersection[j].x << ' ' << perps_intersection[j].y << std::endl;
-            if (belong_to_cell(site[i], perps_intersection[j], perps)) {
-                convex.push_back(perps_intersection[j]);
-            }
-        }
-
-        Cell c(site[i], convex);
-        diagram.push_back(c);
-    }
-
-    std::size_t belong_to_cell(Point site, Point vertex, std::vector<Line> &perps) {
-        std::size_t value = 0;
-        //std::cout << site.x << ' ' << site.y << std::endl;
-        for (std::size_t k = 0; k < perps.size(); k++) {
-            std::size_t a = intersection(perps[k], site, vertex);
-            if (a == 0) {
-                return 0;
-            }
-            value += a;
-        }
-        //std::cout << value << std::endl;
-        //std::cout << std::endl;
-        
-        if (value == perps.size()) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-};
+#define S_SIZE 100
 
 int main() {
     std::vector<Point> site;
@@ -105,15 +12,4 @@ int main() {
     }
     Voronoi V(site);
     V.out();
-    /*std::cout << "Sites:" << std::endl;
-    for (std::size_t i = 0; i < S_SIZE; i++) {
-        std::cout << site[i].x << " " << site[i].y << std::endl;
-
-    }
-    
-    std::cout << "Vertexes:" << std::endl;
-    for (std::size_t i = 0; i < 20; i++) {
-        std::cout << V.perps_intersection[i].x << " " << V.perps_intersection[i].y << std::endl;
-
-    }*/
 }
